d1c8fde40aab165e9489cbe7eba771ce71a38955
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-03-05 17:56:13 +1300

    remove comments

--- include/boot/protectedmode.asm
+++ include/boot/protectedmode.asm
@@ -77,8 +77,6 @@ pm_retrive_video_cursor_settings:
 
 
 
-; eax -> 1 parameter - address of the string to be printed
-;
 ; Each cell on the screen is two bytes. The current byte offset in video
 ; memory can be computed as 0xb8000+(cur_row * screen_width + cur_col) * 2
 pm_display_string:
@@ -192,55 +190,6 @@ set_cursor:
     out dx, al
 
     ret
-
-
-
-
-
-; pm_display_string:
-;     pusha
-;     mov esi, eax                ; Set ESI to beginning of string
-;
-;     ; Assume base of text video memory is ALWAYS 0xb8000
-;     mov ebx, PM.Video_Text.Addr    ; EBX = beginning of video memory
-;
-;     mov eax, [cur_row]          ; EAX = cur_row
-;     mul dword [screen_width]    ; EAX = cur_row * screen_width
-;     mov edx, eax                ; EDX = copy of offset to beginning of line
-;     add eax, [cur_col]          ; EAX = cur_row * screen_width + cur_col
-;     lea edi, [ebx + eax * 2]    ; EDI = memory location of current screen cell
-;
-;     mov ah, PM.Video_Text.Colour ; Set attribute
-;     jmp .getch
-; .repeat:
-;     cmp al, 0xd                  ; Is the character a carriage return?
-;     jne .chk_lf                 ;     If not skip and check for line feed
-;     lea edi, [ebx + edx * 2]    ; Set current video memory pointer to beginning of line
-;     mov dword [cur_col], 0      ; Set current column to 0
-;     jmp .getch                  ; Process next character
-; .chk_lf:
-;     cmp al, 0xa                  ; Is the character a line feed?
-;     jne .write_chr              ;     If not then write character
-;     mov eax, [screen_width]
-;     lea edi, [edi + eax * 2]    ; Set current video memory ptr to same pos on next line
-;     inc dword [cur_row]         ; Set current row to next line
-;     mov ah, PM.Video_Text.Colour ; Reset attribute
-;     jmp .getch                  ; Process next character
-;
-; .write_chr:
-;     inc dword [cur_col]         ; Update current column
-;     stosw
-;
-; .getch:
-;     lodsb                       ; Get character from string
-;     test al, al                 ; Have we reached end of string?
-;     jnz .repeat                 ;     if not process next character
-;
-; .end:
-;     ; call set_cursor             ; Update hardware cursor position
-;
-;     popa
-;     ret
 
 
 pm_endless_loop:
