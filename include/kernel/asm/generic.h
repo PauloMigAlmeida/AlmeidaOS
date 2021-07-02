db02461e9cf05851e363a44393902f928a63f229
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-07-03 11:00:43 +1200

    reformat rdmsr

--- include/kernel/asm/generic.h
+++ include/kernel/asm/generic.h
@@ -11,18 +11,16 @@
 #include "kernel/compiler/freestanding.h"
 #include "kernel/compiler/macro.h"
 
-__force_inline void outb(uint16_t port, uint8_t value)
-{
-    asm volatile (
-        "outb  %[p],  %[v]"
-        :
-        : [p] "Nd" (port), [v] "a" (value)
+__force_inline void outb(uint16_t port, uint8_t value) {
+	asm volatile (
+			"outb  %[p],  %[v]"
+			:
+			: [p] "Nd" (port), [v] "a" (value)
 	);
 }
 
 /* Read MSR specified by ECX into EDX:EAX. */
-__force_inline unsigned long long rdmsr(uint32_t addr)
-{
+__force_inline unsigned long long rdmsr(uint32_t addr) {
 	/*
 	 Reads the contents of a 64-bit model specific register (MSR) specified in
 	 the ECX register into registers EDX:EAX. (On processors that support the
@@ -33,22 +31,21 @@ __force_inline unsigned long long rdmsr(uint32_t addr)
 	 support the Intel 64 architecture, the high-order 32 bits of each of RAX
 	 and RDX are cleared.)
 
-     This instruction must be executed at privilege level 0 or in real-address mode;
-     otherwise, a general protection exception #GP(0) will be generated.
+	 This instruction must be executed at privilege level 0 or in real-address mode;
+	 otherwise, a general protection exception #GP(0) will be generated.
 
 	 The CPUID instruction should be used to determine whether MSRs are supported
 	 (CPUID.01H:EDX[5] = 1) before using this instruction.
 	 */
 
 	unsigned long long retl, reth;
-    asm volatile (
-        "rdmsr \n\t"
-        : "=a" (retl),"=d"(reth)
-        : "c" (addr)
+	asm volatile (
+			"rdmsr \n\t"
+			: "=a" (retl),"=d"(reth)
+			: "c" (addr)
 	);
 
-    return ((unsigned long long)retl)|((reth) << 32);
+	return ((unsigned long long) retl) | ((reth) << 32);
 }
-
 
 #endif /* INCLUDE_KERNEL_ASM_GENERIC_H_ */
