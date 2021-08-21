d3823cea70728671832eef7174fcd3737c3162de
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-08-21 18:45:10 +1200

    implement buddy_free

--- src/kernel/mm/buddy.c
+++ src/kernel/mm/buddy.c
@@ -32,11 +32,11 @@ static void __print_mem_region(mem_map_region_t rg) {
 
 static uint64_t goto_porder_idx(buddy_ref_t *ref, uint8_t pow_order) {
     uint64_t offset_addr = 0;
-    int cond = (ref->max_pow_order - pow_order);
+    int cond = ref->max_pow_order - pow_order;
     for (int i = (ref->max_pow_order - ref->min_pow_order); i > cond; i--) {
-        offset_addr += upow(2, i) * sizeof(buddy_slot_t);
+        offset_addr += upow(2, i);
     }
-    return ref->header_mem_reg.base_addr + offset_addr;
+    return ref->header_mem_reg.base_addr + (offset_addr * sizeof(buddy_slot_t));
 }
 
 __force_inline static uint64_t n_of_entries(buddy_ref_t *ref, uint8_t pow_order) {
@@ -115,7 +115,7 @@ static void insert_slot(buddy_ref_t *ref, buddy_slot_t slot) {
     }
 }
 
-static void remove_slot(buddy_ref_t *ref, buddy_slot_t* slot) {
+static void remove_slot(buddy_ref_t *ref, buddy_slot_t *slot) {
     buddy_slot_t *idx = (buddy_slot_t*) goto_porder_idx(ref, slot->pow_order);
     uint64_t n_entries = n_of_entries(ref, slot->pow_order);
 
@@ -123,10 +123,10 @@ static void remove_slot(buddy_ref_t *ref, buddy_slot_t* slot) {
         buddy_slot_t *tmp = (idx + i);
         if (tmp->base_addr == slot->base_addr) {
 
-            if(i == (n_entries - 1))
+            if (i == (n_entries - 1))
                 memzero(tmp, sizeof(buddy_slot_t));
-            else{
-                memmove(tmp, tmp+1, (n_entries - i) * sizeof(buddy_slot_t));
+            else {
+                memmove(tmp, tmp + 1, (n_entries - i) * sizeof(buddy_slot_t));
             }
 
             break;
@@ -137,9 +137,7 @@ static void remove_slot(buddy_ref_t *ref, buddy_slot_t* slot) {
 void* buddy_alloc(buddy_ref_t *ref, uint64_t bytes) {
     BUG_ON(bytes == 0 || bytes > ref->content_mem_reg.length);
 
-    /* prep */
-    bytes = clp2(bytes);
-    uint8_t k_order = ilog2(bytes);
+    uint8_t k_order = ilog2(clp2(bytes));
 
     /* sanity check */
     if (k_order < ref->min_pow_order)
@@ -153,7 +151,7 @@ void* buddy_alloc(buddy_ref_t *ref, uint64_t bytes) {
         buddy_slot_t *idx = find_free_slot(ref, tmp_k_order);
 
         if (!idx) {
-            /* we've exausted the possibilities, simply there is no memory left */
+            /* we've exausted all possibilities, simply there is no memory left */
             if (tmp_k_order == ref->max_pow_order)
                 break;
 
@@ -169,7 +167,7 @@ void* buddy_alloc(buddy_ref_t *ref, uint64_t bytes) {
             };
 
             buddy_slot_t right = {
-                    .base_addr = idx->base_addr + upow(2, idx->pow_order -1) ,
+                    .base_addr = idx->base_addr + upow(2, idx->pow_order - 1),
                     .pow_order = idx->pow_order - 1,
                     .used = 0
             };
@@ -177,16 +175,16 @@ void* buddy_alloc(buddy_ref_t *ref, uint64_t bytes) {
             insert_slot(ref, left);
             insert_slot(ref, right);
 
-            // remove higher one (old)
+            /* remove higher one (old) */
             remove_slot(ref, idx);
 
-            // now that the split happened, try to acquire the one we are after
+            /* now that the split happened, try to acquire the one we are after */
             tmp_k_order--;
-        }else {
-            // we found the perfect fit \o/
+        } else {
+            /* we found the perfect fit \o/ */
             idx->used = 1;
             found = true;
-            ptr = (uintptr_t*)idx->base_addr;
+            ptr = (uintptr_t*) idx->base_addr;
             break;
         }
     }
@@ -195,6 +193,61 @@ void* buddy_alloc(buddy_ref_t *ref, uint64_t bytes) {
     return ptr;
 }
 
-//void buddy_free(buddy_ref_t *ref, void *ptr) {
-//
-//}
+static buddy_slot_t* find_slot(buddy_ref_t *ref, void *ptr) {
+    buddy_slot_t *slot_found = NULL;
+
+    for (uint8_t i = ref->max_pow_order; i >= ref->min_pow_order; i--) {
+        buddy_slot_t *idx = (buddy_slot_t*) goto_porder_idx(ref, i);
+        uint64_t n_entries = n_of_entries(ref, i);
+
+        for (size_t i = 0; i < n_entries; i++) {
+            buddy_slot_t *tmp = (idx + i);
+            if (is_entry_empty(tmp)) {
+                break;
+            } else if (((uintptr_t*)tmp->base_addr) == ptr) {
+                slot_found = tmp;
+                goto ret;
+            }
+        }
+    }
+
+    ret:
+    BUG_ON(!slot_found);
+    return slot_found;
+}
+
+void buddy_free(buddy_ref_t *ref, void *ptr) {
+    /* sanity check */
+    BUG_ON(!ptr);
+
+    /* free the slot */
+    buddy_slot_t *left_slot = find_slot(ref, ptr);
+    left_slot->used = 0;
+
+    /* check if its buddy is also free so we can merge them */
+    for (uint8_t i = left_slot->pow_order; i < ref->max_pow_order; i++) {
+        char *t_ptr = (char*) ptr;
+        t_ptr += upow(2, left_slot->pow_order);
+        buddy_slot_t *right_slot = find_slot(ref, t_ptr);
+
+        /* if it's still used then party is over */
+        if (right_slot->used)
+            break;
+
+        /* we can merge this one */
+        buddy_slot_t large_slot = {
+                .base_addr = left_slot->base_addr,
+                .pow_order = left_slot->pow_order + 1,
+                .used = 0
+        };
+
+        insert_slot(ref, large_slot);
+        remove_slot(ref, left_slot);
+        remove_slot(ref, right_slot);
+
+        /* feed the logic for higher pow orders recurrence */
+        memcpy(left_slot, &large_slot, sizeof(buddy_slot_t));
+
+    }
+
+}
