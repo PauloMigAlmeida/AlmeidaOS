cb428f991ca640489bc4905514f495cca569db59
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-12-05 16:17:15 +1300

    increase number of blocks allocated for user space program example

--- scripts/raw_disk.sh
+++ scripts/raw_disk.sh
@@ -15,7 +15,7 @@ fi
 # check if user progrm size is bigger then the number of blocks allocated 
 # I will have to use this until I either implement a HD driver and implement a filesystem
 user_size=$(stat -c%s /code/build/user/user)
-if (( user_size > 7680 )); then
+if (( user_size > 10240 )); then
     echo "[ERROR] :: raw_disk.sh :: User program excceded the blocks allocated. Exiting..."
     exit 1
 fi
@@ -24,10 +24,10 @@ rm -f /code/build/disk.img
 dd if=/code/build/boot/mbr.bin of=/code/build/disk.img bs=512 count=1 conv=notrunc
 dd if=/code/build/boot/loader.bin of=/code/build/disk.img bs=512 count=5 seek=1 conv=notrunc
 dd if=/code/build/kernel/kernel of=/code/build/disk.img bs=512 count=152 seek=6 conv=notrunc
-dd if=/code/build/user/user of=/code/build/disk.img bs=512 count=15 seek=158 conv=notrunc
+dd if=/code/build/user/user of=/code/build/disk.img bs=512 count=20 seek=158 conv=notrunc
 
 # this addresses a bug in the qemu that fails to read data out of the disk.img
 # if that terminates prematurely. In a real computer, this wouldn't be likely to
 # happen as (assuming that the usb stick used has a bigger capacity then the file copied),
 # BIOS would read garbage from whatever happens to be on the subsequent blocks.
-truncate -s $(expr 512 \* 173) /code/build/disk.img
+truncate -s $(expr 512 \* 178) /code/build/disk.img
