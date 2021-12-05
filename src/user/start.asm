a75782a64f327fec71231fce8d44eb11be00b04c
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-12-05 16:15:34 +1300

    zero out bss section of user's ELF program

--- src/user/start.asm
+++ src/user/start.asm
@@ -1,19 +1,41 @@
 [BITS 64]
 
+section .data
+
+; Include useful functions, constants and macros
+%include "../../include/boot/global/macro.asm"
+
+
 ; create elf section that is always placed first when linking asm and c files
 section .head.text
 
   ; C-defined functions that this code relies on
-  ;extern memset
   extern umain
 
   ; Export references to C
   global user_entry
 
+  ; Linker-generated symbols
+  extern _BSS_START
+  extern _BSS_SIZE
+
 user_entry:
 
   ; The System V ABI requires the direction flag to be clear on function entry.
   cld
+
+  ; ELF specification dictates that we must clean BSS area before init
+  ;
+  ; .bss: This section holds uninitialized data that contribute to the program's
+  ; memory image. By definition, the system initializes the data with zeros
+  ; when the program begins to run. The section occupies no file space, as
+  ; indicated by the section type, SHT_NOBITS.
+  ;
+  ; https://refspecs.linuxfoundation.org/elf/elf.pdf - Page 29
+  mov rdi, _BSS_START
+  mov rcx, _BSS_SIZE
+  call memzero
+
 
   ; Set %ebp to NULL. This sets a stopping point for coredump functionality when
   ; we try to unwind the call trace to printk the info in the processor at the
@@ -24,7 +46,35 @@ user_entry:
 
   ; we should never ever come back from umain function, but if it does for any
   ; reason, hang the cpu
-  ;jmp endless_loop
-
   .endless_loop:
     jmp .endless_loop
+
+;===============================================================================
+; memzero
+;
+; Fill memory region with zero
+;
+; Inputs:
+;   RDI ->  Buffer address
+;   RCX ->  Amount of bytes to fill
+;
+; Killed registers:
+;   None
+;===============================================================================
+
+memzero:
+  ; preserve all registers
+  pushaq
+
+  ; clear direction flag
+  cld
+
+  ; Define vale to be stored
+  xor   rax,   rax
+
+  ; repeat until ecx is 0
+  rep stosb
+
+  ; restore registers
+  popaq
+  ret
