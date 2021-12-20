028b4e213ee948ebc9644a5c9c7bd27479b0eaf9
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-12-21 08:19:13 +1300

    turn each userspace program a bit more identifiable

--- src/user/main.c
+++ src/user/main.c
@@ -12,18 +12,29 @@
 void umain(void) {
     write("Hello from user space", 22);
 
-    /* get current time */
-    char time_str[50];
-    memset(time_str, '\0', sizeof(time_str));
-    time_t now = time();
-    ltoa(now, time_str, 10);
-    write(time_str, strlen(time_str) + 1);
+    size_t counter = 0;
+    while (1) {
+        counter++;
 
-    /* get current process id */
-    pid_t curr_pid = getpid();
-    char pid_c[2];
-    pid_c[0] = curr_pid + '0';
-    pid_c[1] = '\0';
-    write(pid_c, 2);
+        if (counter % 100000000 == 0) {
+            /* get current time */
+            time_t now = time();
+
+            /* get current process id */
+            pid_t curr_pid = getpid();
+
+            /* print messge */
+            char msg[50];
+            memset(msg, '\0', sizeof(msg));
+            memcpy(msg, "process ", 8);
+            msg[8] = curr_pid + '0';
+            msg[9] = ' ';
+
+            ltoa(now, msg+10, 10);
+
+            write(msg, strlen(msg) + 1);
+        }
+    }
+
 }
 
