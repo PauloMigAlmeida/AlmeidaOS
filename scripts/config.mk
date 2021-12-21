fc16605967bb88b9da02b018e62f5461176892e0
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2021-12-22 11:48:47 +1300

    docs formatting

--- scripts/config.mk
+++ scripts/config.mk
@@ -1,9 +1,9 @@
 #--------------------
 # Project directories
 #--------------------
-DIR_SRC		:= $(DIR_ROOT)/src
+DIR_SRC     := $(DIR_ROOT)/src
 
-DIR_BUILD		:= $(DIR_ROOT)/build
+DIR_BUILD   := $(DIR_ROOT)/build
 DIR_SCRIPTS	:= $(DIR_ROOT)/scripts
 DIR_INCLUDE	:= $(DIR_ROOT)/include
 
@@ -15,30 +15,29 @@ OUTPUT_RAW_DISK := $(DIR_BUILD)/disk.img
 #-------------------
 # Tool configuration
 #-------------------
-TARGET		:= x86_64-elf
+TARGET := x86_64-elf
 
-CC				:= $(TARGET)-gcc
+CC     := $(TARGET)-gcc
 
 # Notes:
 # 	- gnu99 is used instead of c99 so we can use the GCC inline assembly extensions
-CCFLAGS		:= -std=gnu99 -I$(DIR_INCLUDE) -Qn -g \
-		   				-m64 -mno-red-zone -mno-mmx -masm=intel \
-		   				-ffreestanding -fno-asynchronous-unwind-tables \
-		   				-Wall -Wextra -Wpedantic -mcmodel=large -fno-builtin
+CCFLAGS     := -std=gnu99 -I$(DIR_INCLUDE) -Qn -g \
+               -m64 -mno-red-zone -mno-mmx -masm=intel \
+               -ffreestanding -fno-asynchronous-unwind-tables \
+               -Wall -Wextra -Wpedantic -mcmodel=large -fno-builtin
 
-AS				:= nasm
+AS          := nasm
 
-ASFLAGS		:= -f bin
+ASFLAGS     := -f bin
 
+LD          := $(TARGET)-ld
 
-LD				:= $(TARGET)-ld
+LDFLAGS     := -nostdlib -z max-page-size=0x1000
 
-LDFLAGS		:= -nostdlib -z max-page-size=0x1000
+OBJCOPY     := $(TARGET)-objcopy
 
-OBJCOPY		:= $(TARGET)-objcopy
+MAKE_FLAGS  := --quiet --no-print-directory
 
-MAKE_FLAGS	:= --quiet --no-print-directory
+QEMU        := qemu-system-x86_64
 
-QEMU		:= qemu-system-x86_64
-
-GDB			:= gdb
+GDB         := gdb
